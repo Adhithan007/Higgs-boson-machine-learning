31168bef9287cfd51277740fd6974c315243ad98
Adhithan <60425315+Adhithan007@users.noreply.github.com>
2021-10-17 15:37:03 +0530

    Update App.py

--- StreamLit/App.py
+++ StreamLit/App.py
@@ -91,40 +91,55 @@ def main():
         st.pyplot(f)
         
         
+        st.subheader("The basic DL model for the data")
+        with st.echo():
+            # baseline model
+            def create_baseline():
+                # create model
+                model = Sequential()
+                model.add(Dense(60, input_dim=X_train.shape[1], activation='relu'))
+                model.add(Dense(30, activation='relu'))
+                model.add(Dense(2, activation='sigmoid'))
+                # Compile model
+                opt = tf.keras.optimizers.Adam(learning_rate=0.01)
+                model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
+                return model
         
-    
-    st.write('The model was trained on the Higgs Boson dataset')
-    st.subheader('Input the Data')
-    st.write('Please input the data below')
-    colnames=['DER_mass_transverse_met_lep', 'DER_mass_vis', 'DER_pt_h','DER_deltar_tau_lep', 'DER_pt_tot', 'DER_sum_pt','DER_pt_ratio_lep_tau', 'PRI_tau_pt', 'PRI_lep_pt', 'PRI_lep_phi','PRI_met', 'PRI_met_sumet', 'PRI_jet_num', 'PRI_jet_all_pt']
-    
-    v1=st.number_input(colnames[0],)
-    v2=st.number_input(colnames[1],)
-    v3=st.number_input(colnames[2],)
-    v4=st.number_input(colnames[3],)
-    v5=st.number_input(colnames[4],)
-    v6=st.number_input(colnames[5],)
-    v7=st.number_input(colnames[6],)
-    v8=st.number_input(colnames[7],)
-    v9=st.number_input(colnames[8],)
-    v10=st.number_input(colnames[9],)
-    v11=st.number_input(colnames[10],)
-    v12=st.number_input(colnames[11],)
-    v13=st.number_input(colnames[12],)
-    v14=st.number_input(colnames[13],)
-    
-    
-    vals=[v1,v2,v3,v4,v5,v6,v7,v8,v9,v10,v11,v12,v13,v14]
-    
-    
-    if st.button('Detect Event'): 
-      vals= preprocess_test(vals)
-      pred=(np.argmax(model.predict(vals)))
-      st.write("Predicted val")
-      if pred==0:
-        st.success("Predicted val"+" b")
-      else:
-        st.success("Predicted val"+" s")
+        
+    st.button('Wannna test the data'):    
+        st.write('Model used is trained using the above model')
+        st.write('The model was trained on the Higgs Boson dataset')
+        st.subheader('Input the Data')
+        st.write('Please input the data below')
+        colnames=['DER_mass_transverse_met_lep', 'DER_mass_vis', 'DER_pt_h','DER_deltar_tau_lep', 'DER_pt_tot', 'DER_sum_pt','DER_pt_ratio_lep_tau', 'PRI_tau_pt', 'PRI_lep_pt', 'PRI_lep_phi','PRI_met', 'PRI_met_sumet', 'PRI_jet_num', 'PRI_jet_all_pt']
+
+        v1=st.number_input(colnames[0],)
+        v2=st.number_input(colnames[1],)
+        v3=st.number_input(colnames[2],)
+        v4=st.number_input(colnames[3],)
+        v5=st.number_input(colnames[4],)
+        v6=st.number_input(colnames[5],)
+        v7=st.number_input(colnames[6],)
+        v8=st.number_input(colnames[7],)
+        v9=st.number_input(colnames[8],)
+        v10=st.number_input(colnames[9],)
+        v11=st.number_input(colnames[10],)
+        v12=st.number_input(colnames[11],)
+        v13=st.number_input(colnames[12],)
+        v14=st.number_input(colnames[13],)
+
+
+        vals=[v1,v2,v3,v4,v5,v6,v7,v8,v9,v10,v11,v12,v13,v14]
+
+
+        if st.button('Detect Event'): 
+          vals= preprocess_test(vals)
+          pred=(np.argmax(model.predict(vals)))
+          st.write("Predicted val")
+          if pred==0:
+            st.success("Predicted val"+" b")
+          else:
+            st.success("Predicted val"+" s")
       
     
 if __name__ == '__main__':
